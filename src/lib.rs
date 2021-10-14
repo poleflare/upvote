2d1746b060ea713fde6745be6ebd1c7413e28309
Anders Quist <anders@qinfo.se>
2021-10-14 13:55:34 +0200

    chore: fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
@@ -7,10 +7,7 @@ async fn health_check() -> impl Responder {
 }
 
 pub async fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-        })
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .listen(listener)?
         .run();
     Ok(server)
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use upvote::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port");
 
     let server = run(listener).await?;
 
