ffb74566c6ce7c8754ff4c2117f98e1ffb99e0d8
Anders Quist <anders@qinfo.se>
2021-10-26 17:36:55 +0200

    chore: added logging

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A simple upvote REST API.
 
 ## Start the server
-Just run `cargo run` and the server will listen on `127.0.0.1:8000`.
+Just run `RUST_LOG=debug cargo run` and the server will listen on `127.0.0.1:8000`.
 
 ## Run tests
 Tests are run using `cargo test`.
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ use std::net::TcpListener;
 
 use upvote::run;
 
+extern crate log;
+extern crate pretty_env_logger;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    pretty_env_logger::init();
+
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port");
 
     let server = run(listener).await?;
